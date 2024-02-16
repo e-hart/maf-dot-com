7956f234a641f88feb61edbe4fbc003212784935
MildlyAggressiveFolk <ehart.vio@gmail.com>
2024-02-16 13:49:44 -0500

    Fix safari scroll issue ✔️

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,8 @@ import { createContext } from "react";
 import Hero from "~/components/Hero";
 import { Navbar } from "~/components/Navbar";
 import { useRouter } from "next/router";
+import CopyrightFooter from "~/components/CopyrightFooter";
+import SocialFooter from "~/components/SocialFooter";
 
 export const PortalRefContext = createContext<HTMLDivElement | null>(null);
 
@@ -22,14 +24,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <div
-        className={`relative h-screen w-screen overflow-x-clip overflow-y-scroll ${sourceSans3.className}`}
+        className={`relative h-screen w-screen overflow-x-clip overflow-y-scroll ${sourceSans3.className} ${styles.bg}`}
       >
-        <div className={styles.bg} />
         {pathname === "/" ? <Hero /> : null}
-        <div className="slide-from-bottom mx-auto flex w-full flex-col items-center overflow-clip xl:w-[calc(100%-4vw)] xl:max-w-[72rem]">
+        <div className="slide-from-bottom mx-auto flex h-fit min-h-screen w-full flex-col items-center xl:w-[calc(100%-4vw)] xl:max-w-[72rem]">
           <Navbar />
           <Component {...pageProps} />
+          <SocialFooter />
         </div>
+        <CopyrightFooter />
       </div>
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,16 +11,9 @@ const sourceSans3 = Source_Sans_3({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      {/* <div className="slide-from-bottom mx-auto flex w-full flex-col items-center xl:w-[calc(100%-4vw)] xl:max-w-[72rem]">
-        <Navbar /> */}
-      <div className="z-10 w-full bg-white pt-8 text-[#212931] shadow-[-20px_0_20px_-20px_#0008,_20px_0_20px_-20px_#0008]">
-        <Featured />
-        <EmailFooter />
-        <SocialFooter />
-      </div>
-      {/* </div> */}
-      <CopyrightFooter />
+    <main className="z-10 w-full overflow-clip bg-white pt-8 text-[#212931] shadow-[-20px_0_20px_-20px_#0008,_20px_0_20px_-20px_#0008]">
+      <Featured />
+      <EmailFooter />
     </main>
   );
 }
