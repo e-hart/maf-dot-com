776b34abf97e9efd67b3fd219824b972da13fef3
Evan Hart <ehart.vio@gmail.com>
2024-02-16 12:33:51 -0500

    Update CopyrightFooter styling

--- src/components/CopyrightFooter.tsx
+++ src/components/CopyrightFooter.tsx
@@ -2,7 +2,7 @@ export default function CopyrightFooter() {
   return (
     <div
       id="copyright"
-      className="bg w-full bg-[image:linear-gradient(to_right,_#0000_10%,_#0008_50%,_#0000_90%)] px-24 py-12"
+      className="bg w-full bg-[image:linear-gradient(to_right,_#0000_10%,_#0008_50%,_#0000_90%)] py-12 sm:px-24"
     >
       <ul className="letter-spacing flex flex-wrap items-center justify-around text-sm font-black uppercase text-neutral-200">
         <li>&copy; Mildly Aggressive Folk {new Date().getFullYear()}</li>
