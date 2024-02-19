aea09f011de239b3634cd9ac26af900edb9e63a7
MildlyAggressiveFolk <97808888+MildlyAggressiveFolk@users.noreply.github.com>
2024-02-19 17:14:37 -0500

    Update _app.tsx formatting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,6 @@ export default function App({ Component, pageProps }: AppProps) {
         {pathname === "/" && <EmailFooter />}
         <SocialFooter />
       </div>
-
       <CopyrightFooter />
     </div>
   );
