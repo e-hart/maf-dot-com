930a4c5993c7b6891bee5839bb0e2f368688f385
Evan Hart <ehart.vio@gmail.com>
2024-02-19 14:19:15 -0500

    remove outer fragment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,23 +21,21 @@ export default function App({ Component, pageProps }: AppProps) {
   useScrollToTop(pathname, isMobile);
 
   return (
-    <>
-      <div
-        id="wrapper"
-        className={`fixed z-0 h-full w-full overflow-x-clip overflow-y-scroll bg-[image:_linear-gradient(rgba(0,0,0,0.12),rgba(0,0,0,0.12)),url(/images/maf_couch.jpg),url(/images/bg.jpg)] bg-[cover,contain,cover] bg-[position:center,top,center] bg-no-repeat xl:bg-[cover,90%,cover] xl:bg-center ${sourceSans3.className}`}
-      >
-        {pathname === "/" ? <Hero /> : <HeaderBadge />}
-        <div className="slide-from-bottom mx-auto flex h-fit w-full flex-col items-center xl:w-[calc(100%-4vw)] xl:max-w-[72rem]">
-          <Nav />
-          <main className="z-10 w-full overflow-clip bg-white py-24 text-[#212931] shadow-[-20px_0_20px_-20px_#0008,_20px_0_20px_-20px_#0008]">
-            <Component {...pageProps} />
-          </main>
-          {pathname === "/" && <EmailFooter />}
-          <SocialFooter />
-        </div>
-
-        <CopyrightFooter />
+    <div
+      id="wrapper"
+      className={`fixed z-0 h-full w-full overflow-x-clip overflow-y-scroll bg-[image:_linear-gradient(rgba(0,0,0,0.12),rgba(0,0,0,0.12)),url(/images/maf_couch.jpg),url(/images/bg.jpg)] bg-[cover,contain,cover] bg-[position:center,top,center] bg-no-repeat xl:bg-[cover,90%,cover] xl:bg-center ${sourceSans3.className}`}
+    >
+      {pathname === "/" ? <Hero /> : <HeaderBadge />}
+      <div className="slide-from-bottom mx-auto flex h-fit w-full flex-col items-center xl:w-[calc(100%-4vw)] xl:max-w-[72rem]">
+        <Nav />
+        <main className="z-10 w-full overflow-clip bg-white py-24 text-[#212931] shadow-[-20px_0_20px_-20px_#0008,_20px_0_20px_-20px_#0008]">
+          <Component {...pageProps} />
+        </main>
+        {pathname === "/" && <EmailFooter />}
+        <SocialFooter />
       </div>
-    </>
+
+      <CopyrightFooter />
+    </div>
   );
 }
